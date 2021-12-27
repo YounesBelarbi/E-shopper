ae8d1f2b565d6a47e06cbcc062495308ce40fda1
YounesBelarbi <ybelarbi.dev@gmail.com>
2021-12-27 19:44:44 +0100

    style: setting up cart page

---
+++ src/Controller/CartController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/cart", name="cart_")
+ */
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('cart/index.html.twig');
+    }
+}
