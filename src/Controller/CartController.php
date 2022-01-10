0a87f453042f0dbda35100817e7a497f9d3f2459
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-01-10 20:36:15 +0100

    feat: setting up form to cart template

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\CartOrdersFormType;
+use App\Repository\OrdersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -14,8 +18,23 @@ class CartController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Request $request, RequestStack $requestStack, OrdersRepository $ordersRepository): Response
     {
-        return $this->render('cart/index.html.twig');
+
+        $session = $requestStack->getSession();
+
+        if ($session->has('order_id')) {
+            $order = $ordersRepository->find($session->get('order_id'));
+            $form = $this->createForm(CartOrdersFormType::class, $order);
+            $form->handleRequest($request);
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('cart/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
