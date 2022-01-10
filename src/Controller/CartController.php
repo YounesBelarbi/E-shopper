11e6704eb4349fe62f1f479c1b1a709a5e5347e5
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-01-10 20:39:22 +0100

    feat: action to delete orderItem from cart in CartController

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\OrderItem;
 use App\Form\CartOrdersFormType;
 use App\Repository\OrdersRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,5 +38,32 @@ class CartController extends AbstractController
         return $this->render('cart/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(OrderItem $orderItem, Request $request,  ManagerRegistry $doctrine, RequestStack $requestStack, OrdersRepository $orderRepository): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $session = $requestStack->getSession();
+        $currentOrder = $orderRepository->find($session->get('order_id'));
+        $entityManager->remove($orderItem);
+
+        $orderItemTotal = $orderItem->getTotal();
+        $orderTotal = $currentOrder->getTotal();
+
+        $currentOrder
+            ->setTotal($orderTotal - $orderItemTotal);
+
+        $entityManager->flush();
+
+        $productCart = [];
+        foreach ($currentOrder->getOrderItem() as $itemOrder) {
+            $productCart[$itemOrder->getProduct()->getName()] = $itemOrder->getQuantity();
+        }
+
+        $session->set('product_list', $productCart);
+        return $this->redirectToRoute('cart_index');
     }
 }
