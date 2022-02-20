4900cc84a7e6720570c8cce81878506a157dc4a5
YounesBelarbi <ybelarbi.dev@gmail.com>
2022-02-20 21:00:29 +0100

    feat: pagination done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +13,19 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, Request $request): Response
     {
-        return $this->render('home/index.html.twig', [
-            'products' => $productRepository->findAll()
-        ]);
+        //get page from url parameter
+        $currentPage = (int)$request->query->get('page', 1);
+
+        //number of products displayed per page
+        $productPerPage = 9;
+
+        //calculate of the number of pages 
+        $numberTotalOfProducts = $productRepository->getNumberOfProduct();
+        $numberOfPages = ceil((int)$numberTotalOfProducts / $productPerPage);
+        
+        $products = $productRepository->getProducts($currentPage, $productPerPage);
+        return $this->render('home/index.html.twig', compact('products', 'numberOfPages', 'currentPage'));
     }
 }
